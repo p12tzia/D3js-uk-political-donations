7a109f5d653be9e3fc1c876b455a7967e1ff5eec
p12tzia <p12tzia@ionio.gr>
2018-03-13 03:33:04 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -278,11 +278,11 @@ function moveToAmounts(alpha){
 		     var centreY; 
 		     var centreX; 
                  if (d.value <= 100000){	
-			centreX = 200;
-			centreY = 300;
+			centreX = 300;
+			centreY = 400;
 
 		} else if (d.value > 100000 && d.value <=500000){
-                        centreX = 200;
+                        centreX = 300;
 			centreY = 600;
 
 		}  else if(d.value>500000 && d.value<=1000000){
