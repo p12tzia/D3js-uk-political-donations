3c91317cfa4cf4bcea2aae7ed1df5cbf4787e765
p12tzia <p12tzia@ionio.gr>
2018-04-25 18:55:56 +0300

    Update chart2.js

--- chart2.js
+++ chart2.js
@@ -335,6 +335,6 @@ $(document).ready(function() {
       var id = d3.select(this).attr("id");
       return transition(id);
     });
-    return d3.csv("data/7500up.csv", display);
+    return d3.csv("data/Unemployment_rate.csv", display);
 
 });
