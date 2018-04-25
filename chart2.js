2952b3a679214b264b9faf06fefcd70679008ed1
p12tzia <p12tzia@ionio.gr>
2018-04-25 20:19:33 +0300

    Update chart2.js

--- chart2.js
+++ chart2.js
@@ -6,11 +6,11 @@ var force, node, data, maxVal;
 var brake = 0.2;
 var radius = d3.scale.sqrt().range([10, 20]);
 
-var partyCentres = { 
+/*var partyCentres = { 
     con: { x: w / 3, y: h / 3.3}, 
     lab: {x: w / 3, y: h / 2.3}, 
     lib: {x: w / 3	, y: h / 1.8}
-  };
+  };*/
 
 var entityCentres = { 
     company: {x: w / 3.65, y: h / 2.3},
