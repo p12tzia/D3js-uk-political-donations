10f8f49d6cc18dc53919ea88833ba85861a33f1a
p12tzia <p12tzia@ionio.gr>
2018-04-26 04:05:33 +0300

    Update chart2.js

--- chart2.js
+++ chart2.js
@@ -242,7 +242,7 @@ function display(data) {
 
 	var radiusScale = d3.scale.sqrt()
 		.domain([0, maxVal])
-			.range([100, 100]);
+			.range([100, 50]);
 
 	data.forEach(function(d, i) {
 		var y = radiusScale(d.amount);
