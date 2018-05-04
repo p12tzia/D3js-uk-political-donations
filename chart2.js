849bbe58b10bf26c3d41a7982b3b71ff13749359
p12tzia <p12tzia@ionio.gr>
2018-05-04 18:04:56 +0300

    Update chart2.js

--- chart2.js
+++ chart2.js
@@ -152,19 +152,7 @@ function all(e) {
 function moveToCentre(alpha) {
 	return function(d) {
 		var centreX = svgCentre.x + 150;
-			if (d.value <= 5) {
-				centreY = svgCentre.y + 200;
-			} else if (d.value <= 10) {
-				centreY = svgCentre.y + 150;
-			} else if (d.value <= 15) {
-				centreY = svgCentre.y + 100;
-			} else  if (d.value <= 20) {
-				centreY = svgCentre.y + 50;
-			} else  if (d.value <= maxVal) {
-				centreY = svgCentre.y + 20;
-			} else {
-				centreY = svgCentre.y;
-			}
+			
 
 		d.x += (centreX - d.x) * (brake + 0.02) * alpha * 1.2;
 		d.y += (centreY - 100 - d.y) * (brake + 0.02) * alpha * 1.2;
