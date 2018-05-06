8a7eff60c38e6f885f9117c0d9708f69724620a4
p12tzia <p12tzia@ionio.gr>
2018-05-06 05:54:39 +0300

    Update chart2.js

--- chart2.js
+++ chart2.js
@@ -283,7 +283,7 @@ function mouseover(d, i) {
         window.speechSynthesis.speak(speech);
 
 	// image url that want to check
-	var imageFile = "https://raw.githubusercontent.com/p12tzia/D3js-uk-political-donations/master/photos1" + place + ".ico";
+	var imageFile = "https://github.com/p12tzia/D3js-uk-political-donations/tree/master/photos1" + place + ".ico";
 
 	
 	
